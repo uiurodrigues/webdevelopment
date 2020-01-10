5f69868d3712c7ad7e2c0e3419149f14f38fc697
William Rodrigues <wsilva@acesso.com>
2020-01-10 19:22:58 -0300

    first commit

---
+++ curso-react-alura/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
+import Header from './Header';
+import Tabela from './Tabela';
+import Form from './Formulario';
+
+class App extends Component {
+
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Nico',
+        livro: 'Java',
+        preco: '9999'
+      }
+    ],
+  };
+
+  removeAutor = index => {
+    const { autores } = this.state;
+
+    this.setState(
+      {
+        autores: autores.filter((autor, posAtual) => {
+          return posAtual !== index;
+        }),
+      }
+    );
+  }
+
+  escutadorDeSubmit = autor => {
+    this.setState({ autores: [...this.state.autores, autor] })
+  }
+
+  render() {
+    return (
+      <fragment>
+        <Header />
+        <div className="container mb-10">
+          <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+          <Form escutadorDeSubmit={this.escutadorDeSubmit} />
+        </div>
+      </fragment>
+    );
+  }
+}
+
+export default App;
